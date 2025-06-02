59742f830db36b6cdc7424f1523815e6b8d04789
Adam Angelov <Lenchman@Mac-mini.local>
2025-06-02 11:47:26 +0100

    Refactor /gigs to comply with App Router Suspense requirements: moved useSearchParams logic into client component, added Suspense wrapper to page.tsx

--- src/app/clients/[clientName]/page.tsx
+++ src/app/clients/[clientName]/page.tsx
@@ -2,7 +2,6 @@
 
 export const dynamic = "force-dynamic";
 
-"use client";
 import { useGigs } from "@/context/GigsContext";
 import { useClients } from "@/context/ClientsContext";
 import { useParams, useRouter } from "next/navigation";
